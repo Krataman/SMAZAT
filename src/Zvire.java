0f46e0f0440fdc8c29015da2b2b767958b308fe4
Krataman <@>
2024-03-11 08:19:07 +0100

    added Zvire.java

---
+++ src/Zvire.java
@@ -0,0 +1,15 @@
+public abstract class Zvire {
+    protected String druh;
+    protected String jmeno;
+    protected String velikost;
+    protected String specialniPec;
+
+    public Zvire(String druh, String jmeno, String velikost, String specialniPec) {
+        this.druh = druh;
+        this.jmeno = jmeno;
+        this.velikost = velikost;
+        this.specialniPec = specialniPec;
+    }
+
+    public abstract void hladit();
+}
